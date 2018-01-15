05604a1a225aa04513fa91d854486ed4199cf049
Vinay Singh <vinaysingh@ccis.neu.edu>
2018-01-15 07:39:29 -0800

    Q334

---
+++ Q334_Increasing_Triplet_Subsequence.java
@@ -0,0 +1,40 @@
+
+/*
+
+334. Increasing Triplet Subsequence
+
+Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+
+Formally the function should:
+Return true if there exists i, j, k 
+such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+Your algorithm should run in O(n) time complexity and O(1) space complexity.
+
+Examples:
+Given [1, 2, 3, 4, 5],
+return true.
+
+Given [5, 4, 3, 2, 1],
+return false.
+
+*/
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX.VALUE;
+        int c;
+        
+        for (int i = 0; i < nums.length; i++) {
+        	c = nums[i];
+        	if (a >= c) {
+        		a = c;
+        	} else if ( b >= c) {
+        		b =c;        		
+        	} else {
+        		return true;
+        	}
+        }
+        return false;
+    }
+}
